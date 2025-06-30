86f58ad8f79ffb98b7bdb3c5d38a6d4d22413d75
gabicouto93 <gabi.mc.oliveira@gmail.com>
2025-06-30 10:06:33 -0300

    mudanca para o yarn e adicao de reporte

--- tests/teste.test.js
+++ tests/teste.test.js
@@ -1,4 +1,4 @@
-import assert from 'node:assert';
+import { describe, it, expect } from 'vitest'
 
 import {
   geradorDeTagsDeIdentificacao,
@@ -11,24 +11,24 @@ import {
 describe('Testes da disciplina - fundamentos JS', () => {
   
   it('QUANDO informar um nome para o Pet, DEVE ser impresso na tag com letras maiúsculas', () => {
-    assert.strictEqual(geradorDeTagsDeIdentificacao('Pantera'), 'PANTERA');
+    expect(geradorDeTagsDeIdentificacao('Pantera')).toBe('PANTERA');
   });
 
   it('QUANDO a idade = 1 + porte M, DEVE ser permitida a adoção', () => {
-    assert.strictEqual(verificarSePodeSerAdotado(1, 'M'), true)
-  })
+    expect(verificarSePodeSerAdotado(1, 'M')).toBe(true);
+  });
 
   it('QUANDO o peso = 14.5, DEVE ser retornado 4350 gramas para o consumo diário', () => {
-    assert.strictEqual(calcularConsumoDeRacao('Pitoco', 1, 14.5), 4350)
+    expect(calcularConsumoDeRacao('Pitoco', 1, 14.5)).toBe(4350);
   });
 
   it('QUANDO o porte = pequeno, DEVE ser retornada a atividade adequada', () => {
-    assert.strictEqual(decidirTipoDeAtividadePorPorte('pequeno'), 'brincar dentro de casa')
+    expect(decidirTipoDeAtividadePorPorte('pequeno')).toBe('brincar dentro de casa');
   });
 
   it('QUANDO buscar dado de exemplo, DEVE retornar um valor de forma assíncrona', async () => {
     const resultado = await buscarDadoAsync();
-    assert.strictEqual(resultado, 'Pipoca');
+    expect(resultado).toBe('Pipoca');
   });
 
 });
